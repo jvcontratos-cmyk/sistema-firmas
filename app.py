76fc8bb8d6ea3e62370aea720d4c1c46d0999976
jvcontratos-cmyk <jvcontratos@gmail.com>
2026-01-13 21:44:39 -0500

    app.py

--- app.py
+++ app.py
@@ -44,9 +44,9 @@ COORDENADAS_MAESTRAS = {
         7: [(365, 200)]
     },
     "Servicios": {
-        5: [(340, 148)], # Ajusta estos números según veas el cuadro rojo
-        7: [(350, 380)], # Ejemplo: si esta página es un anexo de datos
-        8: [(350, 130)]  # Ejemplo: si esta es la de seguridad
+        5: [(350, 148)], # Ajusta estos números según veas el cuadro rojo
+        7: [(348, 383)], # Ejemplo: si esta página es un anexo de datos
+        8: [(350, 133)]  # Ejemplo: si esta es la de seguridad
     },
 }
 
@@ -131,6 +131,7 @@ with col_visor:
         )
     else:
         st.info("Sube un PDF y dale a 'Procesar' para ver las coordenadas aquí.")
+
 
 
 
